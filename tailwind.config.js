f39323bdf02bff3094508a8d143e9cb03fa05025
kiisi <kiisifelix06@gmail.com>
2025-01-04 22:44:03 +0100

    feat: NWI Online Portal section

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,21 @@ module.exports = {
       fontFamily: {
         "lora": ["Lora", "sans-serif"],
         "open-sans": ["Open Sans", "sans-serif"],
+      },
+      keyframes: {
+        'to-right-from-left': {
+          '49%': {transform: 'translate(100%)'},
+          '50%': {opacity: '0', transform: 'translate(-100%)'},
+          '51%': {opacity: '1'},
+        },
+        "wiggle": {
+          '0%, 100%': { transform: 'rotate(-5deg)' },
+          '50%': { transform: 'rotate(5deg)' },
+        }
+      },
+      animation: {
+        'to-right-from-left': 'to-right-from-left .4s ease-in-out',
+        "wiggle": 'wiggle 1s ease-in-out infinite',
       }
     },
   },
